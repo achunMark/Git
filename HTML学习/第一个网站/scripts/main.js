4bbf6d5026b225635713fc90d817a8940584b91c
AchunMark <sun995025@gmail.com>
2024-08-30 11:50:16 +0800

    8.30第一次提交

---
+++ HTML学习/第一个网站/scripts/main.js
@@ -0,0 +1,12 @@
+let myImage = document.querySelector("img");
+
+myImage.onclick = function () {
+    let mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/1.png") {
+        myImage.setAttribute("src", "images/2.png");
+    } else {
+        myImage.setAttribute("src", "images/1.png");
+    }
+};
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
